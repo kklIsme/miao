67a3d76ff742b5577d9b9ac9ae160740eaa63437
kkl <1040638349@qq.com>
2023-06-02 21:08:24 +0800

    lodash

--- lodash/kklisme-lodash.js
+++ lodash/kklisme-lodash.js
@@ -3,7 +3,7 @@ var kklisme = {
   compact: function (array) {
     var result = []
     for (var item of array) {
-      if (item == false || item == null || item == 0 || item == '' || item == undefined || item == NaN) {
+      if (item == false || item == null || item == 0 || item == '' || item == undefined || isNaN(item)) {
         continue
       } else {
         result.push(item)
@@ -39,6 +39,60 @@ var kklisme = {
   //     }
   //   }
   // },
+
+  // findLastIndex: function (ary, predicate = it => it, fromIndex = 0) {
+  //
+  // },
+
+  flatten: function (ary) {
+    var result = []
+    for(var item of ary){
+      if(Array.isArray(item)){
+        for(var val of item){
+          result.push(val)
+        }
+      }else{
+        result.push(item)
+      }
+    }
+    return result
+  },
+
+
+  flattenDeep: function (ary) {
+    var result = []
+    for(var item of ary){
+      if(Array.isArray(item)){
+        var flattedItem = this.flattenDeep(item)
+        for(var val of flattedItem){
+          result.push(val)
+        }
+      }else{
+        result.push(item)
+      }
+    }
+    return result
+  },
+
+  flattenDepth: function (ary,depth = 1) {
+    if(depth == 0){
+      return ary.slice()
+    }
+    var result = []
+    for(var item of ary){
+      if(Array.isArray(item)){
+        var flattedItem = this.flattenDepth(item,depth - 1)
+        for(var val of flattedItem){
+          result.push(val)
+        }
+      }else{
+        result.push(item)
+      }
+    }
+    return result
+  },
+
+
 
 
 
