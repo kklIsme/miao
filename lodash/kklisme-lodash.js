69da0bc6c4d63caac525fdcbf1e7671ca69ac6d3
kkl <1040638349@qq.com>
2023-06-03 18:19:39 +0800

    lodash

--- lodash/kklisme-lodash.js
+++ lodash/kklisme-lodash.js
@@ -1,35 +1,41 @@
 var kklisme = {
-
   compact: function (array) {
-    var result = []
+    var result = [];
     for (var item of array) {
-      if (item == false || item == null || item == 0 || item == '' || item == undefined || isNaN(item)) {
-        continue
+      if (
+        item == false ||
+        item == null ||
+        item == 0 ||
+        item == "" ||
+        item == undefined ||
+        isNaN(item)
+      ) {
+        continue;
       } else {
-        result.push(item)
+        result.push(item);
       }
     }
-    return result
+    return result;
   },
 
   chunk: function (array, size) {
-    var result = []
+    var result = [];
     for (var i = 0; i < array.length; i += size) {
-      result.push(array.slice(i, i + size))
+      result.push(array.slice(i, i + size));
     }
-    return result
+    return result;
   },
 
   fill: function (array, value, start = 0, end = array.length) {
     for (var i = start; i < end; i++) {
-      array[i] = value
+      array[i] = value;
     }
-    return array
+    return array;
   },
 
-  drop: function (array,n = 1) {
-    array = array.slice(n,array.length)
-    return array
+  drop: function (array, n = 1) {
+    array = array.slice(n, array.length);
+    return array;
   },
 
   // findIndex: function (array, predicate = it => it, fromIndex = 0) {
@@ -45,106 +51,112 @@ var kklisme = {
   // },
 
   flatten: function (array) {
-    var result = []
-    for(var item of array){
-      if(Array.isArray(item)){
-        for(var val of item){
-          result.push(val)
+    var result = [];
+    for (var item of array) {
+      if (Array.isArray(item)) {
+        for (var val of item) {
+          result.push(val);
         }
-      }else{
-        result.push(item)
+      } else {
+        result.push(item);
       }
     }
-    return result
+    return result;
   },
-
 
   flattenDeep: function (array) {
-    var result = []
-    for(var item of array){
-      if(Array.isArray(item)){
-        var flattedItem = this.flattenDeep(item)
-        for(var val of flattedItem){
-          result.push(val)
+    var result = [];
+    for (var item of array) {
+      if (Array.isArray(item)) {
+        var flattedItem = this.flattenDeep(item);
+        for (var val of flattedItem) {
+          result.push(val);
         }
-      }else{
-        result.push(item)
+      } else {
+        result.push(item);
       }
     }
-    return result
+    return result;
   },
 
-  flattenDepth: function (array,depth = 1) {
-    if(depth == 0){
-      return array.slice()
+  flattenDepth: function (array, depth = 1) {
+    if (depth == 0) {
+      return array.slice();
     }
-    var result = []
-    for(var item of array){
-      if(Array.isArray(item)){
-        var flattedItem = this.flattenDepth(item,depth - 1)
-        for(var val of flattedItem){
-          result.push(val)
+    var result = [];
+    for (var item of array) {
+      if (Array.isArray(item)) {
+        var flattedItem = this.flattenDepth(item, depth - 1);
+        for (var val of flattedItem) {
+          result.push(val);
         }
-      }else{
-        result.push(item)
+      } else {
+        result.push(item);
       }
     }
-    return result
+    return result;
   },
 
   fromPairs: function (pairs) {
-    var res = {}
+    var res = {};
 
-    for(var i = 0;i < pairs.length;i++){
-      var key = pairs[i][0]
-      res[key] = pairs[i][1]
+    for (var i = 0; i < pairs.length; i++) {
+      var key = pairs[i][0];
+      res[key] = pairs[i][1];
     }
-    return res
-
+    return res;
   },
 
   toPairs: function (object) {
-    var res = []
-    var obj = Object.keys(object) //Object.keys()静态方法返回一个由给定对象自身的可枚举的字符串键属性名组成的数组。
-    for(var i = 0;i < obj.length;i++){
-      var key = obj[i]
-      res[i] = []
-      res[i][0] = key
-      res[i][1] = object[key]
+    var res = [];
+    var obj = Object.keys(object); //Object.keys()静态方法返回一个由给定对象自身的可枚举的字符串键属性名组成的数组。
+    for (var i = 0; i < obj.length; i++) {
+      var key = obj[i];
+      res[i] = [];
+      res[i][0] = key;
+      res[i][1] = object[key];
     }
 
-    return res
-
+    return res;
   },
 
   head: function (array) {
-    if(!array){
-      return undefined
+    if (!array) {
+      return undefined;
     }
 
-    return array[0]
+    return array[0];
   },
 
   indexOf: function (array, value, fromIndex = 0) {
     var res = 0
-    if(fromIndex >= 0){
-      for(var i = fromIndex;i < array.length;i++){
-        if(array[i] == value){
-          res = i
-          return res
+    if (fromIndex >= 0) {
+        for (var i = fromIndex; i < array.length; i++) {
+            if (array[i] == value) {
+                res = i
+                return res
+            }
         }
+        return -1
+    } else {
+      if(fromIndex % array.length !== 0){
+        return this.indexOf(array, value, fromIndex += array.length)
+      }else{
+        return this.indexOf(array, value, fromIndex = 0)
       }
-      return -1
-    }else{
-      return res - array.length
     }
   },
 
-
-
-
-
-
-
-
-}
+  // lastIndexOf: function (array, value, fromIndex = array.length - 1) {
+  //   var res = 0;
+  //   if (fromIndex >= 0) {
+  //     for (var i = fromIndex; i >= 0; i--) {
+  //       if (array[i] == value) {
+  //         res = i;
+  //         return res;
+  //       }
+  //     }
+  //     return -1;
+  //   }
+  // },
+};
